85d10badf2be64a2f8340525e40ef54022d245f5
Sleepingghost404 <axxinya95@gmail.com>
2024-12-14 12:55:05 +0100

    Add files via upload
    Zaverecny projekt

---
+++ spolecne.js
@@ -0,0 +1,10 @@
+// Select the button and menu elements
+const menuButton = document.querySelector('#menu-tlacitko');
+const menuItems = document.querySelector('#menu-polozky');
+const menuIcon = menuButton.querySelector('i');
+
+menuButton.addEventListener('click', () => {
+  menuItems.classList.toggle('show');
+  menuIcon.classList.toggle('fa-bars');
+  menuIcon.classList.toggle('fa-xmark');
+});
